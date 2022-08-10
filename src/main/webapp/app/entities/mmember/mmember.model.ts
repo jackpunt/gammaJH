a7a7aa393dfd6eeb65b3bdeb4e39280c7ddbb754
jackpunt <jpeckj@gmail.com>
2022-08-10 15:24:50 -0700

    rm files for Mmember, RoleGroup,GroupAuth,Member

--- src/main/webapp/app/entities/mmember/mmember.model.ts
+++
@@ -1,9 +0,0 @@
-import { IRoleGroup } from 'app/entities/role-group/role-group.model';
-
-export interface IMmember {
-  id: number;
-  version?: number | null;
-  roleGroup?: Pick<IRoleGroup, 'id'> | null;
-}
-
-export type NewMmember = Omit<IMmember, 'id'> & { id: null };
--- src/main/webapp/app/entities/role-group/role-group.model.ts
+++
@@ -1,7 +0,0 @@
-export interface IRoleGroup {
-  id: number;
-  version?: number | null;
-  groupName?: string | null;
-}
-
-export type NewRoleGroup = Omit<IRoleGroup, 'id'> & { id: null };
