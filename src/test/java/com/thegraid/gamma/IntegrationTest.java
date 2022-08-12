637947562d39ec06c817cc9f600600cd54cd1402
jackpunt <jpeckj@gmail.com>
2022-08-11 20:57:32 -0700

    Initial version of gammaJH generated by generator-jhipster@7.9.2

---
+++ src/test/java/com/thegraid/gamma/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.thegraid.gamma;
+
+import com.thegraid.gamma.GammaJhApp;
+import com.thegraid.gamma.config.AsyncSyncConfiguration;
+import com.thegraid.gamma.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { GammaJhApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
